52220f269e6c9ca7e6e04b5dc4b8d347f2df08dc
Kasia <katarzyna.tramea@gmail.com>
2021-11-25 13:01:11 +0100

    added menu bar and menu items to GUI

--- src/main/GUI.java
+++ src/main/GUI.java
@@ -7,6 +7,9 @@ public class GUI {
     JFrame window;
     JTextArea textArea;
     JScrollPane scrollPane;
+    JMenuBar menuBar;
+    JMenu menuFile, menuEdit, menuFormat, menuColor;
+    JMenuItem itemNew, itemOpen, itemSave, itemSaveAs, itemExit;
 
     public static void main(String args[]) {
         new GUI();
@@ -15,6 +18,8 @@ public class GUI {
     public GUI() {
         createWindow();
         createTextArea();
+        createMenuBar();
+        createFileMenu();
         window.setVisible(true);
     }
 
@@ -30,5 +35,40 @@ public class GUI {
         scrollPane = new JScrollPane(textArea, JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
         scrollPane.setBorder(BorderFactory.createEmptyBorder());
         window.add(scrollPane);
+    }
+
+    public void createMenuBar() {
+        menuBar = new JMenuBar();
+        window.setJMenuBar(menuBar);
+
+        menuFile = new JMenu("File");
+        menuBar.add(menuFile);
+
+        menuEdit = new JMenu("Edit");
+        menuBar.add(menuEdit);
+
+        menuFormat = new JMenu("Format");
+        menuBar.add(menuFormat);
+
+        menuColor = new JMenu("Color");
+        menuBar.add(menuColor);
+    }
+
+    public void createFileMenu() {
+
+        itemNew = new JMenuItem("New");
+        menuFile.add(itemNew);
+
+        itemOpen = new JMenuItem("Open");
+        menuFile.add(itemOpen);
+
+        itemSave = new JMenuItem("Save");
+        menuFile.add(itemSave);
+
+        itemSaveAs = new JMenuItem("Save as");
+        menuFile.add(itemSaveAs);
+
+        itemExit = new JMenuItem("Exit");
+        menuFile.add(itemExit);
     }
 }
