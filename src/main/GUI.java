982040225f02f02be305d8a100db85a0d8385026
Kasia <katarzyna.tramea@gmail.com>
2021-11-25 11:35:35 +0100

    created gitignore and new class for GUI

---
+++ src/main/GUI.java
@@ -0,0 +1,24 @@
+package main;
+
+import javax.swing.*;
+
+public class GUI {
+
+    JFrame window;
+
+    public static void main(String args[]) {
+        new GUI();
+    }
+
+    public GUI() {
+        createWindow();
+        window.setVisible(true);
+    }
+
+    public void createWindow() {
+        window = new JFrame("Notepad");
+        window.setSize(800, 600);
+        window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+    }
+}
