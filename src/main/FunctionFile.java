bb4b44357db816d679b71a1f23328e44c5728f43
Kasia <katarzyna.tramea@gmail.com>
2021-11-25 13:49:15 +0100

    added opening file function in FunctionFile.java

--- src/main/FunctionFile.java
+++ src/main/FunctionFile.java
@@ -1,10 +1,14 @@
 package main;
 
 import java.awt.*;
+import java.io.BufferedReader;
+import java.io.FileReader;
 
 public class FunctionFile {
 
     GUI gui;
+    String fileName;
+    String fileAddress;
 
     public FunctionFile(GUI gui) {
         this.gui = gui;
@@ -18,5 +22,27 @@ public class FunctionFile {
     public void open() {
         FileDialog fd = new FileDialog(gui.window, "Open", FileDialog.LOAD);
         fd.setVisible(true);
+
+        if(fd.getFile() != null) {
+            fileName = fd.getFile();
+            fileAddress = fd.getDirectory();
+            gui.window.setTitle(fileName);
+        }
+
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(fileAddress + fileName));
+
+            gui.textArea.setText("");
+
+            String line = null;
+
+            while((line = br.readLine()) != null) {
+                gui.textArea.append(line + "\n");
+            }
+            br.close();
+
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
     }
 }
